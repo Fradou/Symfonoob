e33625cdc9ed75fcf7a53fe583e7efa79388b15e
Alexandre FRADET <alex.fradet.1@gmail.com>
2017-03-03 23:56:23 +0100

    Building the sorting by tag function

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,12 +2,31 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('AppBundle:Default:index.html.twig');
+    }
+
+    public function tagSearchAction(Request $request, $tag){
+
+        if ($request->isXmlHttpRequest()) {
+
+            /**
+             * @var $repository ArticleRepository
+             */
+            $repository = $this->getDoctrine()->getRepository('AppBundle:Article');
+            $articles= $repository->getArticleByTag($tag, 5);
+            return new JsonResponse(array("data" => json_encode($articles)));
+
+        } else
+        {
+            throw new HttpException('500', 'Invalid call');
+        }
     }
 }
--- src/AppBundle/Repository/ArticleRepository.php
+++ src/AppBundle/Repository/ArticleRepository.php
@@ -10,4 +10,6 @@ namespace AppBundle\Repository;
  */
 class ArticleRepository extends \Doctrine\ORM\EntityRepository
 {
+    public function getArticleByTag(){
+    }
 }
